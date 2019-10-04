3e810bbf5692e3b6f4931636ae1fdf3250d89c3c
PieroBagaggiolo <56152505+PieroBagaggiolo@users.noreply.github.com>
2019-10-04 16:59:21 +0200

    add class for dinamic operations

---
+++ Calcolatrice/Operation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatrice
+{
+    enum TipoOperazione
+    {
+        Addizione,
+        Sottrazione,
+        Moltiplicazione,
+        Divisione
+    }
+    class Operazione
+    {
+        private int NumElementi { get; set; }
+        TipoOperazione operazione;
+        private int[] Cifre = new int[];
+    }
+}
